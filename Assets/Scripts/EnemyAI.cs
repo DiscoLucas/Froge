6f5e74d76a4a62d078a6ff22ef0b6c5a79a0e929
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2024-04-11 16:16:31 +0200

    Worked on enemy vision function #6

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -12,42 +12,56 @@ public class EnemyAI : MonoBehaviour
     float minWaitTime = 1f;
     float maxWaitTime = 5f;
     bool isWaiting = false;
+    float runSpeed = 10f;
+    float walkSpeed = 5f;
+    float fov = 50f;
+    float viewDistance = 110f;
+
+    Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         Patrol();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        /*
         if (!agent.pathPending && agent.remainingDistance < 0.5f && !isWaiting)
         {
-
+            //Debug.Log("Reached destination");
             // Wait before moving to the next waypoint
             isWaiting = true;
             StartCoroutine(Wait(Patrol));
-        }
-        else if (!isWaiting)
+        }*/
+        
+        Debug.DrawRay(transform.position, transform.forward * viewDistance, Color.red);
+        if (CanSeePlayer())
         {
-            
+            Debug.Log("player is in sight");
         }
-
-
+        else if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                //Debug.Log("Reached destination");
+                // Wait before moving to the next waypoint
+                Patrol();
+            }
     }
 
     /// <summary>
     /// runs a function after a random amount of time
     /// </summary>
-    /// <param name="onComplete"></param>
+    /// <param name="onComplete">This function is run after a randomized period</param>
     /// <returns></returns>
     private IEnumerator Wait(Action onComplete)
     {
 
         float waitTime = UnityEngine.Random.Range(minWaitTime, maxWaitTime);
-        Debug.Log("Waiting " + waitTime);
         
         yield return new WaitForSeconds(waitTime);
         {
@@ -68,13 +82,52 @@ public class EnemyAI : MonoBehaviour
 
     void Patrol()
     {
+        isWaiting = false;
+        agent.speed = walkSpeed;
         // Chek if any waypoints have been set up
         if (waypoints.Length == 0)
+        {
+            Debug.LogWarning("No waypoints set up for enemy");
             return;
+        }
+            
 
         agent.destination = waypoints[destPoint].position;
+        Debug.Log("Going to waypoint " + destPoint);
 
         // Choose the next destination point when the agent gets to one
         destPoint = (destPoint + 1) % waypoints.Length;
+    }
+
+    void Chase()
+    {
+        agent.speed = runSpeed;
+        agent.destination = player.position;
+    }
+    
+    void Attack()
+    {
+        agent.speed = walkSpeed;
+        agent.destination = player.position;
+    }
+
+    //TODO: fix this function so that it can actually see the player
+    bool CanSeePlayer()
+    {
+        Vector3 targetDirection = player.position - transform.position;
+        float angle = Vector3.Angle(transform.forward, targetDirection);
+
+        if (angle < fov * 0.5f)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, targetDirection, out hit, viewDistance))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
